6c0de9887350e3ed3a5b082790ba08bbe03f5228
Juan Carlos <juancagb.17@gmail.com>
2016-11-28 00:19:13 -0500

    Mail layout advances

--- resources/views/layouts/mail.blade.php
+++ resources/views/layouts/mail.blade.php
@@ -26,18 +26,19 @@
         <tr>
             <td align="center">
                 <h2>Quick Deal</h2>
-                {{--<img src="{{ asset('dist/theme/images/logo-home.png') }}" alt="Logo Quick Deal" class="img-center" height="100">--}}
+                @yield('content')
+
+                <div class="divider-quick"></div>
+
+                <p class="text-center">
+                    atentamente el equipo de Quick Deal <br>
+                    wwww.quickdeal.pe
+                </p>
+                <img src="{{ asset('dist/mail/services.png') }}" alt="Services Quick Deal" class="img-center">
+                <img src="{{ asset('dist/mail/footer.png') }}" alt="Footer Quick Deal" class="img-center">
             </td>
         </tr>
     </table>
-    @yield('content')
-
-    <div class="divider-quick"></div>
-
-    <p class="text-center">
-        atentamente el equipo de Quick Deal <br>
-        wwww.quickdeal.com
-    </p>
 
     <!-- JavaScripts -->
     @yield('scripts')
