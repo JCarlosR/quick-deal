157c67427d06a9ce173823254224923186aa142a
Juan Carlos <juancagb.17@gmail.com>
2016-11-28 00:48:50 -0500

    Steps left

--- resources/views/emails/welcome_client.blade.php
+++ resources/views/emails/welcome_client.blade.php
@@ -4,18 +4,19 @@
 <h3 class="text-left">Hola, {{ $name }}</h3>
 <p>Bienvenido a <b>Quickdeal.Pe</b>, la página que te ayudará a encontrar a los mejores profesionales con tan solo un click. Te mostramos una breve instrucción de cómo funciona:</p>
 
-<p>
+<p class="text-left">
     <b>Paso 1:</b> Selecciona el tipo de servicio, fecha y hora que deseas.
 </p>
-<p>
+<p class="text-left">
     <b>Paso 2:</b> Recibirás un mail de confirmación por parte de uno de nuestros proveedores interesados en realizar el servicio que requieres.
 </p>
-<p>
+<p class="text-left">
     <b>Paso 3:</b> Evalúa sus datos personales, experiencia profesional y verifica los comentarios de nuestros sicólogos. Darle click en “confirmar” si deseas que el proveedor realice el servicio, caso contrario, colocar en “cancelar” para que otro de nuestros proveedores pueda visualizar tu requerimiento.
 </p>
-<p>
+<p class="text-left">
     <b>Paso 4:</b> Con la confirmación, liberaremos el correo electrónico y tu número brindado en la página para que el proveedor se contacte contigo de manera directa.
 </p>
+
 <br>
 <p>
     <a href="{{ url('/request') }}">Ingresa a la plataforma</a> para que puedas publicar tu primer requerimiento.
